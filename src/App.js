0406395749da411f3b55e7287300a41ac1e85c7a
Shivam Gupta <er.shivamsg@gmail.com>
2024-02-15 18:44:53 +0530

    medi launching laws page

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import { BrowserRouter as Main, Route, Routes } from "react-router-dom";
+import Home from "./Pages/HomePage/Home";
+import File from "./Pages/Musat/File";
+
+function App() {
+  return (
+    <>
+      <Main>
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/c" element={<File />}></Route>
+        </Routes>
+      </Main>
+    </>
+  );
+}
+
+export default App;
